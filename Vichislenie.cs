b50bd1e39f62fcb4a6acbfe0ae2ad8dc56322a60
kirillgrib4 <kirillgrib4@gmail.com>
2023-01-29 21:05:23 +0400

    c = a+b

---
+++ Vichislenie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+		int a = 1;
+		int b = 2;
+		int c = a + b;
+		Console.WriteLine(c);
+		Console.ReadKey();
+	}
+}
